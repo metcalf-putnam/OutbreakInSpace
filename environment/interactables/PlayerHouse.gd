7a6e8160d48e78b163eb8e8d475b1d38602cd61f
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2020-09-29 08:58:11 -0700

    fixed house exclamation

--- environment/interactables/PlayerHouse.gd
+++ environment/interactables/PlayerHouse.gd
@@ -2,9 +2,19 @@ extends Interactable
 
 func _ready():
 	._ready()
-	$Exclamation.hide()
+	check_status()
+	EventHub.connect("dialogue_finished", self, "check_status")
+
 
 func interact():
 	EventHub.emit_signal("going_in_house_dialogue")
-	pass
-	
+
+
+func check_status():
+	if Global.energy <= 0:
+		has_new_info = true
+		$Exclamation.show()
+	else:
+		has_new_info = false
+		$Exclamation.hide()
+

77dc40dc05fd87abe1199c28182066b2e458de78
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2020-09-29 15:02:30 -0700

    added pet

--- characters/effects/emote.gd
+++ characters/effects/emote.gd
@@ -5,5 +5,10 @@ func emote(type : String):
 	match type:
 		"happy":
 			play("BeatingHeart")
+			print(is_playing())
 		"mad":
 			play("grumbly")
+
+
+func _on_Emote_animation_finished():
+	play("default")
---
+++ characters/pet/pet.gd
@@ -0,0 +1,10 @@
+extends Interactable
+
+
+func _ready():
+	set_new_info(false)
+	$Emote.play("default")
+
+
+func interact():
+	$Emote.emote("happy")

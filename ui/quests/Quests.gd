81ccdfb90fa18509a2033e61de48297ddca94d64
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2020-10-01 22:54:53 -0700

    changed quests scene for better transitions

--- ui/quests/Quests.gd
+++ ui/quests/Quests.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends NinePatchRect
 var completed_modulate = Color(0.58, 0.58, 0.58)
 var full_size = 225
 var mid_size = 137
@@ -12,43 +12,42 @@ func _ready():
 
 
 func _on_TextureButton_toggled(button_pressed):
+	$Label/TextureButton.release_focus()
 	if button_pressed:
-		$Label/List.hide()
+		$List.hide()
 		$Label/TextureButton/Label.text = "show"
 	else:
-		$Label/List.show()
+		$List.show()
 		$Label/TextureButton/Label.text = "hide"
 	update_nine_patch()
 
 
 func update_quests():
 	if !Global.first_lab_visit and Global.player_can_test:
-		$Label.show()
-		$NinePatchRect.show()
+		show()
 	else:
-		$Label.hide()
-		$NinePatchRect.hide()
+		hide()
 		return
 		
 	if Global.idk_quests:
 		all_quests = true
-		$Label/List/IdkQuest1.show()
-		$Label/List/IdkQuest2.show()
-		$Label/List/IdkQuest3.show()
+		$List/IdkQuest1.show()
+		$List/IdkQuest2.show()
+		$List/IdkQuest3.show()
 	else:
 		all_quests = false
-		$Label/List/IdkQuest1.hide()
-		$Label/List/IdkQuest2.hide()
-		$Label/List/IdkQuest3.hide()
+		$List/IdkQuest1.hide()
+		$List/IdkQuest2.hide()
+		$List/IdkQuest3.hide()
 		
-	$Label/List/TestingQuest/Status.bbcode_text = "(" + str(Global.d1s_tested) + "/8)" 
+	$List/TestingQuest/Status.bbcode_text = "(" + str(Global.d1s_tested) + "/8)" 
 	if Global.d1s_tested == 8:
-		strikeout_quest($Label/List/TestingQuest)
+		strikeout_quest($List/TestingQuest)
 	if Global.player_helmet:
-		strikeout_quest($Label/List/HelmetQuest)
-	$Label/List/ConvinceQuest/Status.bbcode_text = "(" + str(Global.npcs_convinced) + "/3)"
+		strikeout_quest($List/HelmetQuest)
+	$List/ConvinceQuest/Status.bbcode_text = "(" + str(Global.npcs_convinced) + "/3)"
 	if Global.npcs_convinced >= 3:
-		strikeout_quest($Label/List/ConvinceQuest)
+		strikeout_quest($List/ConvinceQuest)
 	
 	update_nine_patch()
 
@@ -59,12 +58,12 @@ func _on_dialogue_finished():
 
 func update_nine_patch():
 	if $Label/TextureButton.pressed:
-		$NinePatchRect.rect_size.y = collapsed_size
+		rect_size.y = collapsed_size
 	else:
 		if all_quests:
-			$NinePatchRect.rect_size.y = full_size
+			rect_size.y = full_size
 		else:
-			$NinePatchRect.rect_size.y = mid_size
+			rect_size.y = mid_size
 
 
 func strikeout_quest(node):
